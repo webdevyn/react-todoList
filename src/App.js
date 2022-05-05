b441ef3d26e35255fb3abb4d8ff57bd2722e2ac9
webdevyn <sendtodevyn@gmail.com>
2022-05-05 12:00:23 -0700

    add form to submit new item to task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import ListItem from './ListItem';
+import { useState } from 'react';
 
 function App() {
-  const data = [{id: 1, name: "Pizza"}, {id: 2, name: "Hamburger"}];
-  const listItems = data.map(function(item) {
+  const [tasks, setTasks] = useState([{id: 1, name: "Pizza"}, {id: 2, name: "Hamburger"}])    //tasks is the name of the array
+
+  const [newItemName, setNewItemName] = useState("")
+
+  const listItems = tasks.map(function(item) {
     return <ListItem key={item.id} itemName={item.name}/>
   })
+
+  const handleChange = (event) => {
+    setNewItemName(event.target.value)  //updates state with what user types in
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    //get name from the state --- newItemName
+    //create an object with the name
+    const newTask = {
+      name: newItemName
+    }
+    //add an id to the object using setTasks, can't push to array
+    const lastItemId = tasks[tasks.length - 1].id    //our array is in numeric order
+    newTask.id = lastItemId + 1
+    //make copy of current state
+    const tasksCopy = [...tasks]   //spread operator makes copy of current array
+    //add my object to the copy
+    tasksCopy.push(newTask)
+    //update the state
+    setTasks(tasksCopy)
+    setNewItemName("")
+  }
+
+  const clearList = () => {
+    setTasks([])
+  }
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <form>
-        <label>Name: <input type="text"/></label>
+      <form onSubmit={handleSubmit}>
+        <label>Name: <input onChange={handleChange} value={newItemName} type="text"/></label>
         <input type="submit" value="submit"/>
       </form>
       <ul>
         {listItems}
       </ul>
+      <button onClick={clearList}>Clear List</button>
     </div>
   );
 }
