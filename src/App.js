f01b93f15b86f21c8bfcdc9576a11a6869026812
webdevyn <sendtodevyn@gmail.com>
2022-05-05 10:15:44 -0700

    add html to app.js jsx

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <form>
+        <label>Name: <input type="text"/></label>
+        <input type="submit" value="submit"/>
+      </form>
+      <ul>
+        <li>Pizza</li>
+        <li>Hamburger</li>
+      </ul>
     </div>
   );
 }
